6a0107e4a4dc12147c53949ce09942fb71c2aca9
Orla <orlajcurran@gmail.com>
2025-01-15 16:13:40 +0000

    Added functions to get first 50 pokemon names with api

--- name-that-pokemon-backend/main.py
+++ name-that-pokemon-backend/main.py
@@ -1,8 +1,14 @@
 import requests
 
-pokemonapi_url = "https://pokeapi.co/api/v2/pokemon/ditto"
+pokemonapi_url = "https://pokeapi.co/api/v2/pokemon?limit=50"
 
-pokemonapi_response = requests.get(pokemonapi_url)
-response_json = pokemonapi_response.json()
-pokemon_name = response_json['forms'][0]['name']
+def get_all_pokemon():
+    return requests.get(pokemonapi_url)
+
+pokemon_list_json = get_all_pokemon().json()
+
+def get_pokemon(id):
+    return requests.get(pokemon_list_json['results'][id]['url'])
+
+pokemon_name = get_pokemon(30).json()['forms'][0]['name']
 print(pokemon_name)
